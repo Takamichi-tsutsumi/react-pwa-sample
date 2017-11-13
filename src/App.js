bcf73c7cd651a8d629e01ecd099e191902eeb07e
Takamichi-tsutsumi <takamichi_tsutsumi@waku-2.com>
2017-11-14 01:39:26 +0900

    add lazy loading container

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
 import Media from 'react-responsive'
-import MobileApp from './mobile'
-import DesktopApp from './desktop'
+import Loadable from 'react-loadable';
+
+const Loading = () => {
+  return <div>Loading...</div>;
+};
+
+
+const LoadableMobileApp = Loadable({
+  loader: () => import('./mobile'),
+  loading: Loading,
+});
+
+const LoadableDesktopApp = Loadable({
+  loader: () => import('./desktop'),
+  loading: Loading,
+});
+
 
 class App extends Component {
   render() {
     return (
       <div>
         <Media query={'(max-device-width: 699px)'}>
-          <MobileApp />
+          <LoadableMobileApp />
         </Media>
         <Media query={'(min-device-width: 700px)'}>
-          <DesktopApp />
+          <LoadableDesktopApp />
         </Media>
       </div>
     );
