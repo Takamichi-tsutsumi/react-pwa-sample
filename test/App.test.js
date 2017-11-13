c3c71281acd04cf75b8c8c26afd92f589c541668
Takamichi-tsutsumi <takamichi_tsutsumi@waku-2.com>
2017-11-13 16:05:31 +0900

    add test

--- test/App.test.js
+++ test/App.test.js
@@ -6,3 +6,10 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+it('renders correctly', () => {
+  const wrapper = shallow(
+    <App />
+  );
+  expect(wrapper).toMatchSnapshot();
+});
