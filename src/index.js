4953ce4019008757a813202594fd16fe3916beee
Takamichi-tsutsumi <takamichi_tsutsumi@waku-2.com>
2017-11-22 17:03:06 +0900

    remove sw precache webpack plugin

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { AppRegistry } from 'react-native'
 
 import './index.css'
 import { DesktopApp, MobileApp } from './App'
-import registerServiceWorker from './registerServiceWorker'
 
 if (window.innerWidth < 767) {
   AppRegistry.registerComponent('MobileApp', () => MobileApp)
@@ -14,5 +13,3 @@ if (window.innerWidth < 767) {
 } else {
   ReactDOM.render(<DesktopApp />, document.getElementById('root'))
 }
-
-registerServiceWorker()
