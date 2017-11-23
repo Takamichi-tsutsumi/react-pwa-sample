29877c69fa9d9c9223eef008a7b1bb06f73a4e6c
Takamichi-tsutsumi <takamichi_tsutsumi@waku-2.com>
2017-11-23 20:03:58 +0900

    sw

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -9,6 +9,7 @@ const eslintFormatter = require('react-dev-utils/eslintFormatter')
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin')
 const paths = require('./paths')
 const getClientEnvironment = require('./env')
+const workboxPlugin = require('workbox-webpack-plugin')
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // It requires a trailing slash, or the file assets will get an incorrect path.
@@ -290,7 +291,14 @@ module.exports = {
     // solution that requires the user to opt into importing specific locales.
     // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
     // You can remove this if you don't use Moment.js:
-    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new workboxPlugin({
+      globDirectory: dist,
+      globPatterns: ['**/*.{html,js}'],
+      swDest: path.join(dist, 'sw.js'),
+      clientsClaim: true,
+      skipWaiting: true
+    })
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
--- src/index.js
+++ src/index.js
@@ -13,3 +13,16 @@ if (window.innerWidth < 767) {
 } else {
   ReactDOM.render(<DesktopApp />, document.getElementById('root'))
 }
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then(registration => {
+        console.log('SW registered: ', registration)
+      })
+      .catch(registrationError => {
+        console.log('SW registration failed: ', registrationError)
+      })
+  })
+}
